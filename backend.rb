a0fd7a5e425f292039fe876480475e73b9be6f05
Your Name <abdulkadirazam@gmail.com>
2014-09-20 22:29:58 -0400

    hmm

--- backend.rb
+++ backend.rb
@@ -6,4 +6,5 @@ account_sid = 'PNfe33aca3efd5db370f02103a29ceef0b'
 auth_token = '736020ee82cf40555e6d4033032ddaa9'
 @client = Twilio::REST::Client.new account_sid, auth_token
 @client.account.messages.list.each do |message|
-    File.open(yourfile, 'w') { |file| file.write("#{message}") }
+    File.open("text.txt", 'w') { |file| file.write("#{message}") }
+end
