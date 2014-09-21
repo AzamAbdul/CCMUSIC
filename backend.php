72dbdc522913f3f8bcc87b73abce43faa25f3ba0
Michael Ip <ip.w.michael@gmail.com>
2014-09-20 20:00:59 -0400

    Merge branch 'michael' of https://github.com/AzamAbdul/CCMUSIC into michael
    Conflicts:
    	index.html

---
+++ backend.php
@@ -0,0 +1,6 @@
+<?php
+// Install the library via PEAR or download the .zip file to your project folder.
+// This line loads the library
+require('/path/to/twilio-php/Services/Twilio.php');
+
+mysqli_connect()
