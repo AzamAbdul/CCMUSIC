3176e7b3dde7c1b7dbd977314890036d3f1ad00f
azmabdul <abdulkadirazam@gmail.com>
2014-09-27 14:44:20 -0400

    First commit after hackathon. Cleaning up some of the folder

--- backend.rb
+++ backend.rb
@@ -7,21 +7,26 @@ post '/backend' do
     # Get your Account Sid and Auth Token from twilio.com/user/account
     account_sid = 'AC6c76cd9a15da6f85af498cc6f7bc0199'
     auth_token = '736020ee82cf40555e6d4033032ddaa9'
+    #connects to the mysql database
     con = Mysql.new 'localhost', 'root', 'hackgt', 'votedb'
     @client = Twilio::REST::Client.new account_sid, auth_token
     song =""
     artist =""
     body=""
+    #loop through all the text messages  I have recieved
     @client.account.messages.list.each {|message|
         
+        #parse the message body with regulare expressions
         message.body=~/(^[\s\S]*)by(.*)/
         song=$1
         artist=$2
+        #grab row that has the same song value
         checkexists = con.query "SELECT song FROM votetable WHERE song='#{song}';"
          
         if "#{checkexists.fetch_row}" != "[\"#{song}\"]"
-    	puts "inserted"
-    	con.query("INSERT INTO votetable(song,artist,votes) VALUES('#{song}','#{artist}',1);")
+            #store song into row if it hasnt been stored before
+    	   puts "inserted"
+    	   con.query("INSERT INTO votetable(song,artist,votes) VALUES('#{song}','#{artist}',1);")
          else
     	con.query("UPDATE votetable SET votes=votes+1 WHERE song='#{song}';")
         end
